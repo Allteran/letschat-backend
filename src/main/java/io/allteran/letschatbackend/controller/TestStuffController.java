0f229a162b48e16caab7bce697a31a3421d9e203
Vitalii Prozapas <vitalii.prozapas@gmail.com>
2023-08-26 17:04:03 +0200

    minor fixes

--- src/main/java/io/allteran/letschatbackend/controller/TestStuffController.java
+++ src/main/java/io/allteran/letschatbackend/controller/TestStuffController.java
@@ -1,7 +1,9 @@
 package io.allteran.letschatbackend.controller;
 
+import io.allteran.letschatbackend.domain.Interest;
 import io.allteran.letschatbackend.domain.Role;
 import io.allteran.letschatbackend.domain.User;
+import io.allteran.letschatbackend.service.InterestService;
 import io.allteran.letschatbackend.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -13,6 +15,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.security.Principal;
 import java.time.LocalDateTime;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Set;
 
 @RestController
@@ -20,6 +24,8 @@ import java.util.Set;
 @RequiredArgsConstructor
 public class TestStuffController {
     private final UserService userService;
+    private final InterestService interestService;
+
     @PostMapping("/adc")
     public ResponseEntity<User> createAdmin() {
         User user = new User();
@@ -31,6 +37,34 @@ public class TestStuffController {
         user.setRoles(Set.of(Role.ADMIN, Role.USER));
 
         return ResponseEntity.ok(userService.createUser(user));
+    }
+
+    @PostMapping("/indb")
+    public ResponseEntity<?> fillInterests() {
+        if(interestService.findAll().isEmpty()) {
+            interests().forEach(interestService::create);
+        }
+        return ResponseEntity.ok().build();
+    }
+    private List<Interest> interests() {
+        return Arrays.asList(
+                new Interest("Software"),
+                new Interest("Video games"),
+                new Interest("News"),
+                new Interest("Books"),
+                new Interest("Music"),
+                new Interest("Fashion"),
+                new Interest("Relationships"),
+                new Interest("Education"),
+                new Interest("Finance"),
+                new Interest("Movies"),
+                new Interest("Children"),
+                new Interest("Science"),
+                new Interest("Art"),
+                new Interest("Food"),
+                new Interest("Travel"),
+                new Interest("Psychology")
+        );
     }
 
 }
