da5e577dffaa96c048b72ac1c1fa249c048d4be9
Vitalii Prozapas <vitalii.prozapas@gmail.com>
2024-01-07 12:17:45 +0100

    Minor fixes

--- src/main/java/io/allteran/letschatbackend/dto/payload/UpdatePasswordRequest.java
+++ src/main/java/io/allteran/letschatbackend/dto/payload/UpdatePasswordRequest.java
@@ -1,8 +1,10 @@
 package io.allteran.letschatbackend.dto.payload;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 
 @Data
+@Schema(description = "DTO. Current DTO represent payload for update password requests")
 public class UpdatePasswordRequest {
     private String currentPassword;
     private String newPassword;
