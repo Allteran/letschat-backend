cfba75c762cc8e7f316899e5603b591b79bccc42
Vitalii Prozapas <vitalii.prozapas@gmail.com>
2024-02-18 22:32:01 +0100

    cleanup

--- src/main/java/io/allteran/letschatbackend/config/WebMvcConfig.java
+++ src/main/java/io/allteran/letschatbackend/config/WebMvcConfig.java
@@ -1,10 +1,7 @@
 package io.allteran.letschatbackend.config;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -12,6 +9,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebMvcConfig implements WebMvcConfigurer {
     @Value("${url.frontend}")
     private String URL_FRONTEND;
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
